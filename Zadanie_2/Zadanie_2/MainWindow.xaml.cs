67ca67e369f2c53fbe733f796b18b791d0a9730f
Rafał Lebioda <rafallebioda@gmail.com>
2017-04-19 17:10:31 +0200

    Zadanie 2 init

---
+++ Zadanie_2/Zadanie_2/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Zadanie_2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ObservableCollection<KeyValuePair<double, double>> Seria1 = new ObservableCollection<KeyValuePair<double, double>>();
+        public static Random random = new Random();
+
+
+        public MainWindow()
+        {        
+            InitializeComponent();
+            seria1.DataContext = Seria1;
+            for (int i = 0; i < 10; i++)
+            {
+                Seria1.Add(new KeyValuePair<double, double>(i, i));
+            }
+        }
+    }
+}
---
+++ Zadanie_2/Zadanie_2/Neuron.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_2
+{
+    public class Neuron
+    {
+        public int IloscWejsc { get; set; }
+        public List<double> Wagi { get; set; }
+
+        public Neuron()
+        {
+
+        }
+
+        public Neuron(int iloscWejsc)
+        {
+            IloscWejsc = iloscWejsc;
+            Wagi = new List<double>(iloscWejsc);
+            for (int i = 0; i < Wagi.Capacity; i++)
+            {
+
+                Wagi.Add(MainWindow.random.NextDouble() * 2.0 - 1);
+            }
+        }
+    }
+}
