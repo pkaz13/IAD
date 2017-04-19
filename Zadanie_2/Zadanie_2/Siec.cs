930fcc4b0757b594e12bafa8981e9b879e49af62
Rafał Lebioda <rafallebioda@gmail.com>
2017-04-19 17:22:50 +0200

    Design

---
+++ Zadanie_2/Zadanie_2/Siec.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_2
+{
+    public class Siec
+    {
+
+        public Siec()
+        {
+
+        }
+    }
+}
