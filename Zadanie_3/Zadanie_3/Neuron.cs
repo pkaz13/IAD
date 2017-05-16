4664f603ae383a75cdadf8731964f70aabfd188d
Rafał Lebioda <rafallebioda@gmail.com>
2017-05-16 22:53:02 +0200

    Podstawowe klasy

---
+++ Zadanie_3/Zadanie_3/Neuron.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_3
+{
+    public class Neuron
+    {
+
+
+
+
+        private double FunkcjaAktywacji(double x)
+        {
+            return x;
+        }
+    }
+}
---
+++ Zadanie_3/Zadanie_3/Siec.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_3
+{
+    public class Siec
+    {
+        public List<Neuron> WarstwaWyjsciowa { get; set; } = new List<Neuron>();
+        public List<RadialNeuron> WarstwaUkryta { get; set; } = new List<RadialNeuron>();
+
+    }
+}
